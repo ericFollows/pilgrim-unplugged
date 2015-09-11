15ea5ba3f001012e043ad44d28d87e29ef652332
Eric Follows <livingdesigner@gmail.com>
2015-09-11 17:29:26 +0900

    update to styles

--- routes.js
+++ routes.js
@@ -12,15 +12,30 @@ Router.route('/blog', {
 	template: 'Blog'
 });
 
-Router.route('/blog/:_id', function() {
-	var post = Post.findOne({_id: this.params._id});
-	this.render('Post', {data: post});
-});
+	Router.route('/blog/:_id', function() {
+		var post = Post.findOne({_id: this.params._id});
+		this.render('Post', {data: post});
+	});
 
 Router.route('/gallery', {
 	name: 'gallery',
 	template: 'Gallery'
-})
+});
+
+	Router.route('/gallery/photography', {
+		name: 'photography',
+		template: 'Photography'
+	});
+
+	Router.route('/gallery/graphicdesign', {
+		name: 'graphicDesign',
+		template: 'GraphicDesign'
+	});
+
+	Router.route('/gallery/webdevelopment', {
+		name: 'webDevelopment',
+		template: 'WebDevelopment'
+	});
 
 Router.route('/about', {
 	name: 'about',
